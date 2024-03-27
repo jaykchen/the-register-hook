02b588f8d3e45031c475f39ab2165a85d37124e8
Ji Chen <jaykchen@gmail.com>
2024-03-26 20:04:36 -0400

    simplified

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use std::collections::HashMap;
 use std::env;
 use webhook_flows::{
     create_endpoint, request_handler,
-    route::{get, post, route, RouteError, Router},
+    route::{get, route, RouteError, Router},
     send_response,
 };
 
@@ -33,7 +33,8 @@ async fn handler(
 
     let mut router = Router::new();
     router
-        .insert("/register", vec![get(register_user), post(parse_token)]).unwrap();
+        .insert("/register", vec![get(register_user)])
+        .unwrap();
 
     if let Err(e) = route(router).await {
         match e {
@@ -63,7 +64,6 @@ async fn register_user(
     let token = exchange_token_w_output(code)
         .await
         .expect("failed to get token");
-    log::error!("Token obtained in Register: {:?}", token);
     let user = get_user_profile_with_his_token(&token)
         .await
         .expect("failed to get user profile");
@@ -84,54 +84,29 @@ async fn exchange_token_w_output(code: String) -> anyhow::Result<String> {
         "grant_type": "authorization_code",
     })
     .to_string();
-// "redirect_uri": "https://code.flows.network/webhook/jKRuADFii4naC7ANMFtL/register"
+    // "redirect_uri": "https://code.flows.network/webhook/jKRuADFii4naC7ANMFtL/register"
 
     let writer = github_http_post(&url, &params).await?;
     // "access_token=blurred&scope=read%3Auser&token_type=bearer\"",
     let stuff_in_writer = String::from_utf8_lossy(&writer);
     log::error!("Exchange token Response: {:?}", stuff_in_writer);
-    let parsed: Vec<(String, String)> = form_urlencoded::parse(writer.as_slice())
-        .into_owned()
-        .collect();
-    let temp_str = String::new();
-    let mut token = parsed
-        .iter()
-        .find(|(k, _)| k == "access_token")
-        .map(|(_, v)| v.to_string())
-        .unwrap_or(temp_str);
 
-    log::error!("Token parsed from params: {:?}", token.clone());
+    let load: Load = serde_json::from_slice(&writer)?;
 
-    if token.is_empty() {
-        let load: Load = match serde_json::from_slice(&writer) {
-            Ok(m) => {
-                log::error!("{m:?}");
-                m
-            }
-
-            Err(e) => {
-                log::error!("failed to parse access token response: {:?}", e);
-                panic!()
-            }
-        };
-        #[derive(Debug, Deserialize, Serialize, Clone, Default)]
-        struct Load {
-            access_token: Option<String>,
-            scope: Option<String>,
-            token_type: Option<String>,
-        }
-        log::error!("Token parsed from bdoy: {:?}", token.clone());
-
-        match load.access_token {
-            Some(m) => token = m,
-            None => {
-                log::error!("failed to get token");
-                panic!()
-            }
-        }
+    #[derive(Debug, Deserialize, Serialize, Clone, Default)]
+    struct Load {
+        access_token: Option<String>,
+        scope: Option<String>,
+        token_type: Option<String>,
     }
 
-    Ok(token.to_string())
+    match load.access_token {
+        Some(m) => Ok(m),
+        None => {
+            log::error!("failed to get token");
+            anyhow::bail!("failed to get token")
+        }
+    }
 }
 
 pub async fn github_http_post(url: &str, query: &str) -> anyhow::Result<Vec<u8>> {
@@ -210,54 +185,4 @@ pub async fn get_user_profile_with_his_token(
     let twitter_username = user.twitter_username.unwrap_or_default();
     let email = user.email.unwrap_or_default();
     Ok((name, login, twitter_username, email))
-}
-
-/* async fn exchange_token(code: String) -> anyhow::Result<()> {
-    let client_id = env::var("client_id").expect("github_client_id is required");
-    let client_secret = env::var("client_secret").expect("github_client_secret is required");
-    let url = format!("https://github.com/login/oauth/access_token");
-    // let url = format!("https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}&redirect_uri=https://code.flows.network/webhook/jKRuADFii4naC7ANMFtL/register");
-
-    let params = json!({
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "code": code,
-        "redirect_uri": "https://code.flows.network/webhook/jKRuADFii4naC7ANMFtL/register"
-    })
-    .to_string();
-
-    log::error!("URL: {:?}", url);
-    let _ = github_http_post(&url, &params).await?;
-    Ok(())
-} */
-
-async fn parse_token(
-    _headers: Vec<(String, String)>,
-    _qry: HashMap<String, Value>,
-    _body: Vec<u8>,
-) {
-    log::error!("post query: {:?}", _qry);
-    // let mut token = String::new();
-    // match _qry.get("access_token") {
-    //     Some(m) => token = m.as_str().unwrap_or_default().to_owned(),
-    //     _ => log::error!("missing code"),
-    // };
-
-    #[derive(Debug, Deserialize, Serialize, Clone, Default)]
-    struct Load {
-        access_token: Option<String>,
-        scope: Option<String>,
-        token_type: Option<String>,
-    }
-
-    let load: Load = serde_json::from_slice(&_body).unwrap();
-
-    let token = load.access_token.unwrap_or_default();
-
-    log::error!("Token: {:?}", token);
-    let user = get_user_profile_with_his_token(&token)
-        .await
-        .expect("failed to get user profile");
-
-    log::error!("User: {:?}", user);
 }
